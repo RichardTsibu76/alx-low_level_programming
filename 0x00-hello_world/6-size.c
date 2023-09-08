6332831e1d2f1dde409569b307d58abe4724a307
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-09-08 14:06:33 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 
-	printf("Size of a float: %f byte(s)\n", sizeof(float));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
 	return (0);
 }
