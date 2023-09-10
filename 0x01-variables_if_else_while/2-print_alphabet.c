aa4132c46f230580a8c64832588afbe02a4ed416
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-09-10 10:49:30 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - This is the main entry
+ *Return: Always return 0.
+ */
+int main(void)
+{
+	char letters;
+
+	for (letters = 'a'; letters <= 'z'; letters++)
+	{
+		putchar(letters);
+
+	}
+	putchar('\n');
+
+	return (0);
+}
