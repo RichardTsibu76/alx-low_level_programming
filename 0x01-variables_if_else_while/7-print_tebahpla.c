c51fb6763907d0deff80694284f46a81116552ba
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-09-11 17:35:30 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.>
+
+/**
+ *main - This is the main entry.
+ *Return: always return 0.
+ */
+int main(void)
+{
+
+	char i;
+
+	for (i = 'z'; i >= 'a'; i++)
+	{
+
+		putchar (i);
+	}
+		putchar ('\n');
+		return (0);
+}
