39484d5e1debae6b1afca06df70c7d5e82259ef6
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-09-14 16:04:38 -0700

    ./3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *print_numbers - this prints numbers
+ *Return - Nothing
+ */
+void print_numbers(void)
+{
+	char fig;
+
+	for (fig = '0'; fig <= '9'; fig++)
+	{
+		_putchar (fig);
+	}
+	_putchar(10);
+
+}
