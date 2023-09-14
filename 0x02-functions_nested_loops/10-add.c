0cae5d930660d027b3f92be72355a47e9b088d3e
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-09-13 22:50:55 -0700

    ./10-add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,10 +3,11 @@
 
 /**
  *add -  This adds the fuction and return result
- *Description: the addition and returning of result
+ *@a: first integer
+ *@b: second integer
  *Return: return addition of a and b
  */
-int add(int a, int b )
+int add(int a, int b)
 {
 
 	return (a + b);
