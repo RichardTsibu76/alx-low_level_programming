b09a9e06ed84c816d525282dc5b840b64ed51375
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-09-13 22:48:07 -0700

    ./10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *add -  This adds the fuction and return result
+ *Description: the addition and returning of result
+ *Return: return addition of a and b
+ */
+int add(int a, int b )
+{
+
+	return (a + b);
+}
