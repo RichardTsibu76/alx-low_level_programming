0b00d5003c57fb033baf9e01d8e8f3d9d4665b6a
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-09-14 18:16:28 -0700

    ./5-more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *more_numbers - Displaying the figures 10 times
+ *Return: Nothing
+ */
+void more_numbers(void)
+{
+	int num, digit;
+
+	for (num = 0; num <= 9; num++)
+	{
+
+
+			for (digit = 0;  digit <= 14; digit++)
+			{
+				if (digit > 9)
+				_putchar((digit / 10) + '0');
+				_putchar((digit % 10) + '0');
+
+			}
+	_putchar(10);
+	}
+
+}
