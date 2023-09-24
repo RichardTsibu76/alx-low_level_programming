95adfc40268664f98a51f4a5fc456dede13c67aa
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-09-23 21:17:56 -0700

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ *rot13 - This function encodes a string using rot13
+ *@str: This is a string to encode
+ *Return: Returns
+ */
+char *rot13(char *str)
+{
+	int i;
+	int j;
+
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char b[] = "nopqrstuvwxyzabcdefghijkklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	for (i = 0; *(str + i); i++)
+	{
+	
+		for (j = 0; j < 52; j++)
+		{
+			if (a[j] == *(str + j))
+			{
+				*(str + i) = b[j];
+				break;
+			}
+		
+		}
+	
+	}
+	return (str);
+}
