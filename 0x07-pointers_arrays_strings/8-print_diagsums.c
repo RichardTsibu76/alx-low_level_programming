2e5054f3fb06beb69bc6fc81257e41a1a782ab59
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-09-25 13:55:42 -0700

    8-print_diagsums.c

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_diagsums - This function prints the sum of two diagonals of sqr matrix
+ *@a: a pointer
+ *@size: size prior to array
+ *Return: returns nothing
+ */
+void print_diagsums(int *a, int size)
+{
+	int row, column, sum1 = 0, sum2 = 0;
+
+	for (row = 0; row < size; row++)
+	{
+		for (column = 0; column < size; column++)
+		{
+			if (column == row)
+			{
+				sum1 += *(a + (row * size) + column);
+
+			}
+		}
+
+	}
+	printf("%d,  ", sum1);
+
+	for (row = 0; row < size; row++)
+	{
+		for (column = 0; column < size; column++)
+		{
+			if ((row + column) == (size - 1))
+				sum2 += *(a + (row * size) + column);
+		}
+	}
+	printf("%d\n", sum2);
+}
+
