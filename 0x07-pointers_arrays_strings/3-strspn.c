db1dd67241e1bd794d6bbb6825077198e854dfdc
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-09-25 21:50:21 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,25 +8,23 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, int j;
+	int i;
+	int j;
 	unsigned int result = 0;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; s[i] != '\0'; j++)
+		for (j = 0; s[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				result++;
 				break;
-
 			}
-
 		}
 
 	if (s[j] == '\0')
 		return (result);
-
 	}
 	return (result);
 }
