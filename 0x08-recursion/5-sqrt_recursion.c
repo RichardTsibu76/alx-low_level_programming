b87286e26faa8b62856ab932622875d6cb17e8b3
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-09-28 10:58:59 -0700

    5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+int _helper_function(int n, int root);
+/**
+ * _sqrt_recursion - This funtion returns natural sqr root
+ *@n: intput
+ *@root: square root checker
+ *@Return: base on condition
+ */
+int _sqrt_recursion(int n)
+{
+	int root = 0;
+
+	if (n < 0)
+		return (-1);
+	if (n == 1)
+		return (1);
+
+	return (_helper_function(n, root));
+}
+/**
+ * _helper_function - This funct finds sq root
+ * @root: square root as parameter
+ * @n: number of emphasis
+ * Return: root
+ */
+int _helper_function(int n, int root)
+{
+	if (n == root * root)
+		return (root);
+	if (root == (n / 2))
+		return (-1);
+	else
+	return (_helper_function(n, root + 1));
+}
