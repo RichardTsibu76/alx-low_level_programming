93b294799beba12432437e0afa670f5cf0440650
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-10-05 21:20:09 -0700

    reallocation size in bytes

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_reallocc - this fuction reallocates memory block
+ *@ptr: the pointer
+ *@old_size - old mem size
+ *@new_size: new mem size to be reallocated
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	int *nptr, *p;
+	unsigned int c;
+
+	if (old_size == new_size)
+	{
+		return (ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	p = ptr;
+	nptr = malloc(new_size);
+	if (nptr == NULL)
+	{
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		return (nptr);
+	}
+	if (new_size > old_size)
+	{
+		for (c = 0; c < old_size; c++)
+		{
+			nptr[c] = p[c];
+		}
+	}
+	else
+	{
+		for (c = 0; c < new_size; c++)
+		{
+			nptr[c] = p[c];
+		}
+	}
+	free(ptr);
+	return (nptr);
+}
