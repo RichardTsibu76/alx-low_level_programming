3654e173d77df094061140f02912731bfa982e50
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-10-07 10:09:14 -0700

    memory allocation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,18 +10,16 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int idx;
-	char* array;
+	char *array;
 
 	if (size == 0)
 	return (NULL);
-	array = (char*) malloc(sizeof(char)* size);
+	array = (char *) malloc(sizeof(char) * size);
 	if (array != NULL)
 	{
 		for (idx = 0; idx < size; idx++)
 			*(array + idx) = c;
-		
 	}
-	free(array);
 	return (array);
 
 }
