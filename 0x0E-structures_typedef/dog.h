32204ee1ca628b30f910afed380294ff94e421ff
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-10-10 11:33:26 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *struct dog - This is the struct datatype
+ *@name: The name of the element in struct
+ *@age: The age
+ *@owner: possesion
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
