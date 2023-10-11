87d9ef9ea7d3bd130deab6d1dbacccabb995e9bd
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-10-11 16:29:45 -0700

    checking array elements

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -3,15 +3,16 @@
 /**
  *array_iterator - executes a given function
  *@size: array size
+ *@array: a pointer to array
  *@action: a pointer to function needed
  *Return: nothing
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t array_index;
+	size_t index;
 
 	if (array == NULL || size <= 0 || action == NULL)
 		return;
-	for (array_index = 0; array_index < size; array_index++)
-		action(array[array_index]);
+	for (index = 0; index < size; index++)
+		action(array[index]);
 }
