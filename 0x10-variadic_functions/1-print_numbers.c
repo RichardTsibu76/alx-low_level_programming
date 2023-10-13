ff898067c14d165ad4435d2c489a2be3d61a1adf
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-10-12 22:35:18 -0700

    1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,25 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers
+ * @separator: delimeter
+ * @n: arguments to print
+ * Return: returns
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list print_args;
+	unsigned int idx = 0;
+
+	va_start(print_args, n);
+	while (idx < n)
+	{
+		printf("%d", va_arg(print_args, int));
+		if (idx < (n - 1) && separator != NULL)
+			printf("%s", separator);
+		idx++;
+	}
+	printf("\n");
+	va_end(print_args);
+}
