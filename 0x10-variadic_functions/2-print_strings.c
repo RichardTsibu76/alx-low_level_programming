9e3689a6234e92694e763d02b85562b05e3ef88f
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-10-12 22:54:20 -0700

    variadic function

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,5 +1,7 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+#include <stddef.h>
+#include <stdio.h>
 /**
  * print_strings - prints strings followed by new line
  * @separator: this is more like delimeter
