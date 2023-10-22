2db3f43601723e5a6d8aacbe3ba459c24b211a1e
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-10-22 11:23:45 -0700

    struct

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,7 +1,28 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * struct dog - The structure defines the elements of a dog
+ * @name: The element name of type char
+ * @age: The element age of type float
+ * @owner: The element owner of type char
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
