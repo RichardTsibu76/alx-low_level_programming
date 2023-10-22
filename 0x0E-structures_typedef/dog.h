8a9959ab8499003596eadb36992b36cd62a988b2
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-10-22 11:27:59 -0700

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,28 @@
-#ifndef STRUCT_DOG_H
-#define STRUCT_DOG_H
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- *struct dog - This is the struct datatype
- *@name: The name of the element in struct
- *@age: The age
- *@owner: possesion
+ * struct dog - The structure defines the elements of a dog
+ * @name: The element name of type char
+ * @age: The element age of type float
+ * @owner: The element owner of type char
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
