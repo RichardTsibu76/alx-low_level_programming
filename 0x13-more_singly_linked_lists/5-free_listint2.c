e2799e4e26d4418a4fd9bb4f722fd1c3a7d6a926
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-10-24 08:11:22 -0700

    task five

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,18 +7,18 @@
  */
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp = (*head);
+	listint_t *tmp;
 
-	if (*head == NULL)
-
-	return;
-
-	while (*head != NULL)
+	if (head != NULL)
 	{
-		tmp = (*head)->next;
+		while ((*head)->next != NULL)
+		{
+			tmp = (*head)->next;
+			free(*head);
+			*head = tmp;
+		}
 		free(*head);
-		(*head) = tmp;
+		*head = NULL;
 	}
-	(*head) = NULL;
 
 }
