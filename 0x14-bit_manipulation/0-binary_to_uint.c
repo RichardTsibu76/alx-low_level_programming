52c19e7d4e171dc0d12f9284ff7a23cdbbb3c1fc
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-10-26 14:21:41 -0700

    bitwise

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,20 +10,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int binary_to_uint(const char *b)
-	{
-		unsigned int idx = 0, base10 = 0;
+	unsigned int idx = 0, base10 = 0;
 
-		if (!b)
+	if (!b)
+		return (0);
+	while (b[idx])
+	{
+		if (b[idx] == '1' || b[idx] == '0')
+			base10 = (base10 << 1) + (b[idx] - '0');
+		else
 			return (0);
-		while (b[idx])
-		{
-			if (b[idx] == '1' || b[idx] == '0')
-				base10 = (base10 << 1) + (b[idx] - '0');
-			else
-				return (0);
-			idx++;
-		}
-		return (base10);
+		idx++;
 	}
+	return (base10);
+
 }
