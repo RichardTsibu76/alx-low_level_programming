53d74f036cf05121bf02dba3e944ff028f736984
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-10-27 05:32:10 -0700

    task zero

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,11 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- *hese are the prototype of the fucntions
- *the header files that are here also
- *
- */
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -16,5 +11,4 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
-
-#endif                                
+#endif
