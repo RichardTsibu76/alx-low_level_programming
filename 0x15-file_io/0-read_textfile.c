78fdf4a3c069a51f1717a8e21723b6c9aab96ba2
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-10-30 15:40:29 -0700

    files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,22 +8,22 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t fd, r_byte, w_byte;
+	ssize_t file_d, r_byte, w_byte;
 	char *str;
 
-	fd = open(filename, O_RDWR);
+	file_d = open(filename, O_RDWR);
 
-	if ((filename == NULL) || (fd < 0))
+	if ((filename == NULL) || (file_d < 0))
 		return (0);
 	str = malloc((letters) * sizeof(char));
 	if (str == NULL)
 		return (0);
-	r_byte = read(fd, str, letters);
+	r_byte = read(file_d, str, letters);
 	w_byte = write(STDOUT_FILENO, str, r_byte);
 	if (w_byte < 0)
 		return (0);
 	free(str);
-	close(fd);
+	close(file_d);
 	return (r_byte);
 
 
