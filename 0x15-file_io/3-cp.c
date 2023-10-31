15b9e59085643c309cb4fd3ff553a3b8c30b8a35
RICHARD <paakwesi5976@gmail.com>
2023-10-31 01:44:12 +0100

    copies file content to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,11 +29,11 @@ int main(int argc, char *argv[])
  */
 void copy_from_to(const char *file_from, const char *file_to)
 {
-	int filedes, r_byte, w_byte, c1_byte, c2_byte;
+	int fd_f, fd_t, r_byte, w_byte, c1_byte, c2_byte;
 	char *buff;
 
-	filedes = open(file_from, O_RDONLY);
-	if ((filedes < 0) || (file_from == NULL))
+	fd_f = open(file_from, O_RDONLY);
+	if ((fd_f < 0) || (file_from == NULL))
 	{
 		dprintf(2, "Error: Can't read from file %s\n", file_from);
 		exit(98);
@@ -41,35 +41,35 @@ void copy_from_to(const char *file_from, const char *file_to)
 	buff = malloc(sizeof(char) * BUFFER);
 	if (buff == NULL)
 		exit(98);
-	r_byte = read(filedes, buff, BUFFER);
+	r_byte = read(fd_f, buff, BUFFER);
 	if (r_byte < 0)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
-	filedes = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (filedes < 0)
+	fd_t = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (fd_t < 0)
 	{
 		dprintf(2, "Error: Can't write to %s\n", file_to);
 		exit(99);
 	}
-	w_byte = write(filedes, buff, r_byte);
+	w_byte = write(fd_t, buff, r_byte);
 	if (w_byte < 0)
 	{
 		dprintf(2, "Error: Can't write to %s\n", file_to);
 		exit(99);
 	}
 	free(buff);
-	c1_byte = close(filedes);
+	c1_byte = close(fd_f);
 	if (c1_byte < 0)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", filedes);
+		dprintf(2, "Error: Can't close fd %d\n", fd_f);
 		exit(100);
 	}
-	c2_byte = close(filedes);
+	c2_byte = close(fd_t);
 	if (c2_byte < 0)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", filedes);
+		dprintf(2, "Error: Can't close fd %d\n", fd_t);
 		exit(100);
 	}
 	chmod(file_to, 0664);
