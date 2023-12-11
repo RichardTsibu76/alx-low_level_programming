31693014742656bd294c2c3e31dfaee262f4a9bf
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-12-11 19:33:01 +0100

    Create a dynamic library that contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/arithemetic_for_add_sub_div_mod_multply.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+/**
+*add  - This adds two numbers
+*@a: the first num
+*@b: the second number
+*Return: a + b
+*/
+int add(int a, int b)
+{
+	return a + b;
+
+}
+
+/**
+*div - this functions divides two num
+*@a: num 1
+*@b: num 2
+*Return: a / b
+*/
+int div(int a, int b)
+{
+	return a / b;
+}
+/**
+*mul - this functions multiplies two numbers
+*@a: num 1
+*@b: num 2
+*Return: a * b
+*/
+int mul(int a, int b)
+{
+	return a * b;
+}
+
+/**
+*sub - This functions takes the difference of two numbers
+*@a: The first number
+*@b. The second number
+*Return: This returns the differece of two nums
+*/
+int sub(int a, int b)
+{
+	return a - b;
+}
+/**
+*mod - Remainder
+*@a: First num
+*@b: The second num
+*Return: Returns the remainder
+*/
+int mod(int a, int b)
+{
+	return a % b;
+}
