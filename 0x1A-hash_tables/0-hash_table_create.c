caae19c56c787107b177bfa48441d25caf59d913
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-12-23 04:36:33 +0100

    hash table task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,29 +5,19 @@
  * @size: size of array for the hash table
  * Return: on success, it points to the newly created h table
  */
-
-hash_table_t *hash_table_create(unsigned long int size)
+sh_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
-	unsigned long int i = 0;
+	hash_table_t *new_table = NULL;
 
-	if (size == 0)
+	new_table = malloc(sizeof(hash_table_t));
 		return (NULL);
-	hash_table = malloc(sizeof(hash_table_t));
-	if (hash_table == NULL)
+	new_table->size = size;
+
+	new_table->array = malloc(sizeof(void *) * size);
 	{
+		free(new_table);
 		return (NULL);
 	}
-	hash_table->array = malloc(sizeof(hash_table_t *) * size);
-		if (hash_table->array == NULL)
-		{
-			return (NULL);
-		}
 
-	for (i = 0; i != size; i++)
-	{
-		hash_table->array[i] = NULL;
-	}
-	hash_table->size = size;
-	return (hash_table);
+	return (new_table);
 }
