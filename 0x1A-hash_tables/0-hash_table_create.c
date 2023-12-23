1b5ed9226ed526ade5fdb5acc3e2ced79d3441ea
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-12-23 01:49:08 +0100

    hash table task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,7 +14,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table = (hash_table_t *) malloc(sizeof(hash_table_t));
 	if (table == NULL)
-		free(table);
 		return (NULL);
 
 	array = (hash_node_t **) malloc(sizeof(hash_node_t *) * size);
