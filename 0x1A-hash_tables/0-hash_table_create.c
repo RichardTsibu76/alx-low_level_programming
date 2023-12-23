6d14afac8dec117f72f42d1199c61f1aac9e5a9f
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2023-12-23 04:38:57 +0100

    hash table task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -5,15 +5,20 @@
  * @size: size of array for the hash table
  * Return: on success, it points to the newly created h table
  */
-sh_table_t *hash_table_create(unsigned long int size)
+
+hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table = NULL;
 
+	/* allocate new table struct */
 	new_table = malloc(sizeof(hash_table_t));
+	if (!new_table) /* if malloc fails */
 		return (NULL);
 	new_table->size = size;
 
+	/* allocare array of pointers */
 	new_table->array = malloc(sizeof(void *) * size);
+	if (!new_table->array) /* if array alloc fails */
 	{
 		free(new_table);
 		return (NULL);
