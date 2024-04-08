c6e0931dff633ac49dc393ad560589a80e3a8ee4
Richard Tsibu Darkoh <paakwesi5976@gmail.com>
2024-04-08 19:12:41 +0200

    Task 0 of search algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -5,26 +5,26 @@
  *integers using the Linear search algorithm
  *
  *@array: input array
- *@size: size of the array
- *@value: value to search in
+ *@size: size of the array of the algorithm
+ *@value: The value to search
  *Return: Always EXIT_SUCCESS
  */
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t x;
+	size_t v;
 
 	if (array == NULL)
 	{
 		return (-1);
 	}
 
-	for (x = 0; x < size; x++)
+	for (v = 0; v < size; v++)
 	{
-		printf("Value checked array[%li] = [%i]\n", x, array[x]);
-		if (array[x] == value)
+		printf("Value checked array[%li] = [%i]\n", v, array[v]);
+		if (array[v] == value)
 		{
-			return (x);
+			return (v);
 		}
 	}
 	return (-1);
